a30f993d5dd9d442794d02b016ca82065d565050
Kaze Wong <kazewong.physics@gmail.com>
2025-03-19 12:55:55 -0400

    Implement scroll-based animation and display scroll values

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,26 +4,51 @@
     import { onMount } from 'svelte';
     import anime from 'animejs';
 
-    let myDiv;
+    let scrollY = $state(0);
+    let old_scrollY = $state(0);
 
-    onMount(() => {
-        anime.timeline({loop: true}).add({
-            targets: myDiv,
-            translateX: 250,
-            rotate: '1turn',
-            backgroundColor: '#FF0000',
-            duration: 800,
-            easing: 'easeInOutQuad'
+    let trigger = $state(false);
+
+    let text = 'The Straight';
+
+    $effect(() => {
+        if (scrollY < 100) {
+        anime({
+            targets: text,
+            opacity: [0, 1],
+            duration: 1000,
+            easing: 'easeInOutQuad',
         });
+    };
     });
 </script>
 
-<div bind:this={myDiv} style="width: 100px; height: 100px; background-color: #0000FF;"></div>
+<svelte:window on:scroll={() => {
+    scrollY = window.scrollY;
+    if (scrollY > old_scrollY) {
+        trigger = true;
+    } else {
+        trigger = false;
+    }
+    old_scrollY = scrollY;
+    }
+} />
+<header class="fixed top-0 left-0 right-0 z-50 bg-black shadow-md">
+    <div class="container mx-auto px-4">
+        <div class="py-4">
+            <p class="text-center text-lg">Scroll Y: {scrollY}</p>
+            <p class="text-center text-lg">Old Scroll Y: {old_scrollY}</p>
+            <p class="text-center text-lg">Trigger: {trigger}</p>
+
+        </div>
+    </div>
+</header>
+
 
 <div class="hero bg-red-400 min-h-screen">
     <div class="hero-content text-center">
       <div class="max-w-md">
-        <h1 class="text-5xl font-bold">The Straight</h1>
+        <h1 class="text-5xl font-bold" bind:this={text}>The Straight</h1>
       </div>
     </div>
 </div>
