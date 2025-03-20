81c3ae8b5f7a555f40c2abe49ba32249de114eb7
Kaze Wong <kazewong.physics@gmail.com>
2025-03-19 20:55:27 -0400

    Clean up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,19 +3,15 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import anime from 'animejs';
-	import { on } from 'svelte/events';
 
   let pageHeight = 0;
 
   onMount(() => {
     pageHeight = document.body.clientHeight;
-    console.log(pageHeight);
   });
 
 
   let scrollY = $state(0);
-  let old_scrollY = $state(0);
-  let trigger = $state(false);
 
   let video: HTMLVideoElement;
   let text = 'The Straight';
@@ -47,33 +43,26 @@
 
 <svelte:window on:scroll={() => {
     scrollY = window.scrollY;
-    if (scrollY > old_scrollY) {
-        trigger = true;
-    } else {
-        trigger = false;
-    }
-    old_scrollY = scrollY;
-    }
-} />
 
-<header class="fixed flex items-center justify-center h-screen mb-12 overflow-hidden">
+    }} />
 
+<header class="fixed flex-row items-center justify-center h-screen mb-12 overflow-hidden">
+  <div class="container mx-auto px-4">
+    <div class="py-4">
+        <p class="text-center text-lg">Scroll Y: {scrollY}</p>
+
+
+    </div>
+</div>
   <video class="min-h-screen h-screen" bind:this={video} src="/test.mp4" type="video/mp4"></video>
     <!-- <source bind:this={video} src="/test.mp4" type="video/mp4" controls> -->
   <!-- </video> -->
   
 </header>
 
-<!-- <header class="fixed top-0 left-0 right-0 z-50 bg-black shadow-md">
-    <div class="container mx-auto px-4">
-        <div class="py-4">
-            <p class="text-center text-lg">Scroll Y: {scrollY}</p>
-            <p class="text-center text-lg">Old Scroll Y: {old_scrollY}</p>
-            <p class="text-center text-lg">Trigger: {trigger}</p>
+<header class="fixed top-0 left-0 right-0 z-50 bg-black shadow-md">
 
-        </div>
-    </div>
-</header> -->
+</header>
 
 
 <div class="hero bg-red-400 min-h-screen">
