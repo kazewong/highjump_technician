b71e721fbeeae1e11abe819186a23723b3d12948
Kaze Wong <kazewong.physics@gmail.com>
2025-03-28 22:12:14 -0400

    Refactor scroll handling and improve image loading logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,9 @@
-
-
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import anime from 'animejs';
+	import { onMount } from 'svelte';
+	import anime from 'animejs';
 
-  import test_image from '$lib/assets/straight/ezgif-frame-017.jpg';
-  let pageHeight = 0;
+	import test_image from '$lib/assets/straight/ezgif-frame-017.jpg';
+	let pageHeight = 0;
 
 	const imageModules = import.meta.glob(
 		'$lib/assets/straight//*.{avif,gif,heif,jpeg,jpg,png,tiff,webp,svg}',
@@ -15,41 +13,42 @@
 				enhanced: true
 			}
 		}
-  )
-  console.log(imageModules);
-  console.log(imageModules[Object.keys(imageModules)[0]]);
+	);
+	console.log(imageModules);
 
-  let scrollY = $state(0);
-  let heading = $state("The Straight");
+	let scrollProgress = $state(0);
+	let heading = $state('The Straight');
+	const scrollSensitivity = 0.01;
 
-
-  onMount(() => {
-    pageHeight = document.body.clientHeight;
-  });
-
+	function handleWheel(event) {
+		scrollProgress += event.deltaY * scrollSensitivity;
+		scrollProgress = Math.max(0, Math.min(100, scrollProgress));
+	}
+	onMount(() => {
+		pageHeight = document.body.clientHeight;
+	});
 </script>
 
-<svelte:window on:scroll={() => {
-    scrollY = window.scrollY;
+<svelte:window on:wheel={handleWheel} />
 
-    }} />
-
-<header class="fixed flex-row items-center justify-center h-screen mb-12 overflow-hidden">
-  <div class="container mx-auto px-4">
-    <div class="py-4">
-        <p class="text-center text-lg">Scroll Y: {scrollY}</p>
-    </div>
-  </div>
+<header class="fixed mb-12 h-screen flex-row items-center justify-center overflow-hidden">
+	<div class="container mx-auto px-4">
+		<div class="py-4">
+			<p class="text-center text-lg">Scroll Y: {scrollProgress}</p>
+		</div>
+	</div>
 </header>
 
-<enhanced:img src={imageModules[Object.keys(imageModules)[25]].default} alt="Preloaded Image" class="fixed top-0 left-0 w-full h-full object-cover z-0" />
+<enhanced:img
+	src={imageModules[Object.keys(imageModules)[25]].default}
+	alt="Preloaded Image"
+	class="fixed top-0 left-0 -z-5 h-full w-full object-cover"
+/>
 
-<div class="hero bg-red-400 min-h-screen">
-  
-  <div class="hero-content text-center">
-    <div class="max-w-md">
-      <h1 class="text-5xl font-bold">{heading}</h1>
-    </div>
-  </div>
+<div class="hero bg-opacity-0 min-h-screen">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">{heading}</h1>
+		</div>
+	</div>
 </div>
-
